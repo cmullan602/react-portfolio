1f30ab0fcf615af0f035b12f788f77d86be5faf3
Connor Mullan <cmullan602@gmail.com>
2022-10-20 15:42:34 -0700

    added images and card.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import FixedBottomNavigation from './components/botomNav';
 import './App.css';
 import ResponsiveAppBar from './components/navbar';
+import ActionAreaCard from './components/projectCards';
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
      <p>Portfolio </p>
+     <ActionAreaCard />
      <FixedBottomNavigation />
     </div>
   );
